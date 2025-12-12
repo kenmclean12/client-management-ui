a0421c27ce7e0d872457a018795ef53f6a8bfb5c
kenmclean12 <kenmclean20@gmail.com>
2025-12-12 15:46:57 -0800

    1

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SnackbarProvider } from "notistack";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <SnackbarProvider
+        maxSnack={3}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        style={{ zIndex: 999999 }}
+        preventDuplicate
+      >
+        <App />
+      </SnackbarProvider>
     </QueryClientProvider>
   </StrictMode>
 );
