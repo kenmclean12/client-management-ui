1b6d0c5f817891b1010df1caeb977ebe03f88781
kenmclean12 <kenmclean20@gmail.com>
2025-12-15 23:01:19 -0800

    1

--- src/components/Page/Placeholder/styles.ts
+++ src/components/Page/Placeholder/styles.ts
@@ -18,3 +18,13 @@ export const avatarStyles = {
   height: 100,
   bgcolor: "grey.400",
 };
+
+export const textStyles = {
+  color: "grey",
+  fontWeight: 600,
+}
+
+export const subTextStyles = {
+  color: "grey",
+  textAlign: "center",
+};
