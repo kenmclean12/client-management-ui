e0ad463df38de4a5674e32fddc32bc19dfbbe331
kenmclean12 <kenmclean20@gmail.com>
2025-12-16 16:52:47 -0800

    1

--- src/components/Project/ProjectUpdateDialog/index.tsx
+++ src/components/Project/ProjectUpdateDialog/index.tsx
@@ -87,7 +87,7 @@ export function ProjectUpdateDialog({ project }: Props) {
             >
               {Object.entries(ProjectStatus)
                 .filter(
-                  ([key, val]) => typeof val === "number" && key !== "Approved"
+                  ([key, val]) => typeof val === "number" && key !== "Done"
                 )
                 .map(([key, val]) => (
                   <MenuItem key={val} value={val}>
